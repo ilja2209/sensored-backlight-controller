aa1cf7ab794edf2e9426011ff4ba40feff268c21
root <root@DESKTOP-CAILO9I.localdomain>
2021-04-04 22:13:54 +0300

    first commit

---
+++ led-controller.ino
@@ -0,0 +1,78 @@
+#include <Ultrasonic.h>
+
+#define SENSOR_POWER_PIN 9
+#define SENSOR_SIG 7
+#define LED_PWM_PIN 5
+#define PWM_INTERVAL 5
+#define SWITCHING_DISTANCE 65
+#define TIME_BEFORE_SWITCHING_OFF 300000 //5 min
+#define MAX_BRIGHT 255
+#define MIN_BRIGHT 0
+
+Ultrasonic ultrasonic(SENSOR_SIG);
+volatile bool isSwitchedOn = false;
+volatile int filterOneSwitchOnCounter = 0;
+volatile int filterOneSwitchOffCounter = 0;
+ 
+void setup() {
+  pinMode(SENSOR_POWER_PIN, OUTPUT);
+  digitalWrite(SENSOR_POWER_PIN, HIGH);
+  analogWrite(LED_PWM_PIN, 0);
+  isSwitchedOn = false;
+  Serial.begin(9600);
+}
+
+void switchOn() {
+  for (int i = MIN_BRIGHT; i <= MAX_BRIGHT; i++) {
+    delay(PWM_INTERVAL);
+    analogWrite(LED_PWM_PIN, i);
+  }
+}
+
+void switchOff() {
+  for (int i = MAX_BRIGHT; i >= MIN_BRIGHT; i--) {
+    delay(PWM_INTERVAL);
+    analogWrite(LED_PWM_PIN, i);
+  }
+}
+
+long measureDistance() {
+    long distance = ultrasonic.MeasureInCentimeters();
+    Serial.print(distance);
+    Serial.println(" cm");
+    return distance;
+}
+
+bool filterOne(int *counter, bool flag) {
+  if (flag) {
+    if (++*counter < 7) {
+      return false;
+    } else {
+      return true;
+    }
+  } else {
+    *counter = 0;
+    return false;
+  }
+}
+
+void loop() {
+  
+  if (filterOne(&filterOneSwitchOnCounter, measureDistance() < SWITCHING_DISTANCE)) {
+    if (!isSwitchedOn) {
+      Serial.println("Switch on");
+      switchOn();
+      isSwitchedOn = true;
+    }
+  } 
+  
+  if (filterOne(&filterOneSwitchOffCounter, measureDistance() >= SWITCHING_DISTANCE)) {
+    if (isSwitchedOn) {
+      delay(TIME_BEFORE_SWITCHING_OFF);
+      Serial.println("Switch off");
+      switchOff();
+      isSwitchedOn = false;
+    }
+  }  
+  delay(200);
+}
